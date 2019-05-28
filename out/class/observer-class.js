1772ae57f6f1ed7d677d0d4cc7315211944ea3db
Dominic Pohland <dpohland@bossinfo.ch>
2019-05-28 16:52:59 +0200

    Update OOP implementation

--- out/class/observer-class.js
+++ out/class/observer-class.js
@@ -1,6 +1,43 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode = require("vscode");
 class ObserverFile {
     constructor(documentTitle) {
     }
+    setDocumentTitle(documentTitle) {
+        this.documentTitle = documentTitle;
+    }
+    getDocumentTitle() {
+        return this.documentTitle;
+    }
+    setText(text) {
+        this.text = text;
+    }
+    getText() {
+        return this.text;
+    }
+    setSetting(settingParam) {
+        var setting = vscode.Uri.parse("untitled:" + this.getDocumentTitle);
+        vscode.workspace.openTextDocument(setting).then((document) => {
+            vscode.window.showTextDocument(document, 1, false).then(e => {
+                e.edit(edit => {
+                    edit.insert(new vscode.Position(0, 0), this.getText());
+                });
+            });
+        }, (error) => {
+            console.error(error);
+        });
+    }
 }
+// var setting: vscode.Uri = vscode.Uri.parse("untitled:" + documentTitle);
+// 	vscode.workspace.openTextDocument(setting).then((tableDoc: vscode.TextDocument) => {
+// 		vscode.window.showTextDocument(tableDoc, 1, false).then(e => {
+// 			e.edit(edit => {
+// 				edit.insert(new vscode.Position(0, 0), tableText);
+// 			});
+// 		});
+// 	}, (error: any) => {
+// 		console.error(error);
+// 		debugger;
+// 	});	
 //# sourceMappingURL=observer-class.js.map
--- src/class/observer-class.ts
+++ src/class/observer-class.ts
@@ -1,8 +1,51 @@
+import * as vscode from 'vscode';
+import { CreateObserverTable } from '../observerFunctions';
+
 class ObserverFile {
     documentTitle!: string;
     setting!: string;
+    text!: string;
 
     constructor(documentTitle: string){
 
     }
+
+    setDocumentTitle(documentTitle: string){
+        this.documentTitle = documentTitle;
+    }
+    getDocumentTitle(){
+        return this.documentTitle;
+    }
+    setText(text: string){
+        this.text = text;
+    }
+    getText(){
+        return this.text;
+    }
+    
+    setSetting(settingParam: string){
+        var setting: vscode.Uri = vscode.Uri.parse("untitled:" + this.getDocumentTitle);
+            vscode.workspace.openTextDocument(setting).then((document: vscode.TextDocument) => {
+                vscode.window.showTextDocument(document, 1, false).then(e => {
+                    e.edit(edit => {
+                        edit.insert(new vscode.Position(0,0), this.getText());
+                    });
+                });
+            }, (error: any) => {
+                console.error(error);
+            });
+    }
 }
+
+
+// var setting: vscode.Uri = vscode.Uri.parse("untitled:" + documentTitle);
+// 	vscode.workspace.openTextDocument(setting).then((tableDoc: vscode.TextDocument) => {
+// 		vscode.window.showTextDocument(tableDoc, 1, false).then(e => {
+// 			e.edit(edit => {
+// 				edit.insert(new vscode.Position(0, 0), tableText);
+// 			});
+// 		});
+// 	}, (error: any) => {
+// 		console.error(error);
+// 		debugger;
+// 	});	
